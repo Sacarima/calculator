44862cfd4c6dc710892c8f3986add823fb35f379
Sacarima <caroline12alex@yahoo.com>
2023-04-26 23:56:41 +0200

    display config, omit operator from displaying when press js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const screen = document.querySelector('.calculator-screen')
 
 //target the keys and dsplay on the screen
 function getDigits() {
-    // using closest to make sure the program runs only if button are
+    // using closest to make sure the program runs only if buttons are
     //clicked
 
     if(!event.target.closest('button')) return
@@ -18,4 +18,15 @@ function getDigits() {
     // a variable is created outside this function.(the variable needs to be overwritten by the numbers or operators we click)
     const screenValue = screen.textContent
     screen.textContent = keyValue
+
+    // at this point numbers appear when click, but its replaced when another number or operator is clicked. no change this behavior the following is needed
+    if(screenValue === '0') /*we check for string 0 '0'*/{
+        screen.textContent = keyValue
+    }else {
+        screen.textContent = screenValue + keyValue
+    }
+
+    /*this part will omit the operator from being displayed on the screen
+    if it's an operator it's treated differently, the same if it's a number should be treared differently
+    */
 }
